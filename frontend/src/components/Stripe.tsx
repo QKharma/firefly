9b49305720d210473a5a786955283b84f4a87479
Yumi <yuma.gugerli@gmail.com>
2023-04-04 20:43:02 +0200

    :woozy: Uhm

---
+++ frontend/src/components/Stripe.tsx
@@ -0,0 +1,9 @@
+let Stripe = () => {
+  return (
+    <div className='p-2 border-2 m-2 flex justify-center'>
+      <span>test</span>
+    </div>
+  )
+}
+
+export default Stripe
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,3 +1,12 @@
+import Stripe from '@/components/Stripe'
+
 export default function Home() {
-  return <main className='text-white'>helo</main>
+  return (
+    <main className='text-white'>
+      <div className='grid grid-cols-4'>
+        <Stripe />
+        <Stripe />
+      </div>
+    </main>
+  )
 }
